545c277b8e887b08b557c6f7a8e34f2b49496cfd
mrlanu <mrlanu@gmail.com>
2019-08-21 19:20:58 -0500

    completed stage #1

--- Game of Life/task/src/life/Main.java
+++ Game of Life/task/src/life/Main.java
@@ -1,7 +1,25 @@
 package life;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int S = scanner.nextInt();
+
+        Random random = new Random(S);
+
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                if(random.nextBoolean()){
+                    System.out.print("O");
+                }else System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
